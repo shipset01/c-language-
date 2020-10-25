04231558b0f90a017b5fa148cf083724f2c458bb
shipset01 <73393803+shipset01@users.noreply.github.com>
2020-10-25 17:47:23 +0100

    Add files via upload

---
+++ conf.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+void line(), message(); // Prototypes
+
+int main(void)
+{
+
+/*ping www.google.co.ma ;*/
+int a,dd,n,i;
+double xa,ifconfig=0 ;
+  //bool ifconfig ;
+ printf ("hello user \n");
+if (ifconfig == 0)
+  {
+    printf ("net down \n");
+   ifconfig==1;}
+xa=3.14;
+a=6;
+printf ("xa=%f et a= %d \n",xa , a);
+scanf("%d",&dd);
+ printf ("dd=%d \n",dd);
+ i=1;
+for (i=1; i<=4; i++)
+{
+
+printf ("test is working \n ");
+if (xa==3.14)
+printf ("pi is valid \n ");}
+printf("ifconfig=%.0f",ifconfig);
+return 0;
+getch();
+}
+void message() // To display a message.
+{
+printf("In function message().");
+}
