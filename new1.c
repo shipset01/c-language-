5cfcce4f76984c683a12d539d6223d56dc2e60bc
shipset01 <73393803+shipset01@users.noreply.github.com>
2020-11-09 19:53:04 +0100

    Update new1.c

--- new1.c
+++ new1.c
@@ -5,6 +5,7 @@ A program with some functions and comments
 using namespace std;
 void line(), message(); // Prototypes
 int main()
+  // open files --->inputs & results
 {
 cout << "Hello! The program starts in main()."
 << endl;
@@ -14,11 +15,12 @@ line();
 cout << "At the end of main()." << endl;
 return 0;
 }
+// add function 
 void line() // To draw a line.
 {
 cout << "--------------------------------" << endl;
 }
-void message() // To display a message.
+void message() // To display a test message.
 {
 cout << "In function message()." << endl;
 }
